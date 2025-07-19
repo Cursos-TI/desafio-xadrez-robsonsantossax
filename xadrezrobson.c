aee31a47c7ecbef790886fe8cec7d216131109e3
robsonsantossax <robsonsax@hotmail.com>
2025-07-19 14:50:53 -0300

    Update xadrezrobson.c
    movimentação do bispo finalizada

--- xadrezrobson.c
+++ xadrezrobson.c
@@ -1,25 +1,26 @@
-#include <stdio.h>
-
 // Desafio de Xadrez - Robson Gari dos Santos
 
 
 int main() {
     // Nível Novato - Movimentação das Peças
 
- 
-
-// Utilizando uma estrutura de repetição para simular a movimentação do Bispo em diagonal
+ // Implementação de Movimentação do Bispo
+ // Utilizando uma estrutura de repetição para simular a movimentação do Bispo em diagonal
  // Movimentação de 5 casa para a direção diagonal esquerda
 
-printf("Implementação de Movimentação do Bispo - Movimentação de 5 casa para a direção diagonal esquerda\n")
+
 int b = 1;
 
-while (b < = 5)
+printf("Implementação de Movimentação do Bispo - Movimentação de 5 casas para a direção diagonal esquerda\n");
+
+while (b <= 5)
 {
-    printf("o bispo moveu 1 casa para esquerda e 1 casa para cima\n", b); 
+    printf("O bispo moveu 1 casa para esquerda e 1 casa para cima\n", b); 
 
     b++;
 }
+
+// Implementação de Movimentação do Bispo
 
     return 0;
 
