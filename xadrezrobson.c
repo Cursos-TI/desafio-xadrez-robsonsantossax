8a97664ec26f5312a51a3c55abe7fbf2c7de889c
robsonsantossax <robsonsax@hotmail.com>
2025-07-19 14:45:04 -0300

    Update and rename xadrerobson.c to xadrezrobson.c

---
+++ xadrezrobson.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Desafio de Xadrez - Robson Gari dos Santos
+
+
+int main() {
+    // Nível Novato - Movimentação das Peças
+
+ printf("Implementação de Movimentação do Bispo\n")
+
+// Utilizando uma estrutura de repetição para simular a movimentação do Bispo em diagonal
+ // Movimentação de 5 casa para a direção diagonal esquerda
+
+int b = 1;
+
+while (b < = 5)
+{
+    printf("o bispo moveu 1 casa para esquerda e 1 casa para cima\n", b); 
+
+    b++;
+}
+
+    return 0;
+
+}
